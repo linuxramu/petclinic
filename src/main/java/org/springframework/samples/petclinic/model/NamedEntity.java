a0cb39af47caa97b8a1b9f29b4f4eb6f80985e0d
linuxramu <linuxramu@gmail.com>
2025-02-01 18:16:47 +0000

    named entity . java

--- src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+//dummy code
 package org.springframework.samples.petclinic.model;
 
 import jakarta.persistence.Column;
