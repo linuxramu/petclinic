f42f4d7f35dcb381ce489842c2995536729300bb
linuxramu <linuxramu@gmail.com>
2025-02-01 18:45:21 +0000

    person

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//dummy
+
 package org.springframework.samples.petclinic.model;
 
 import jakarta.persistence.Column;
