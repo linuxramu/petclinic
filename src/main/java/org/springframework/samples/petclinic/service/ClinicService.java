5a6064d8ff31c11a9ad924b8509dd16a149da847
linuxramu <linuxramu@gmail.com>
2025-02-02 17:53:16 +0000

    dummt code

--- src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// code dummy
 package org.springframework.samples.petclinic.service;
 
 import java.util.Collection;
