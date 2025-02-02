d6ee2d5edfacb5eb72102b69a697e3e3f90a27c8
linuxramu <linuxramu@gmail.com>
2025-02-02 18:03:31 +0000

    fixed typo

--- src/main/java/org/springframework/samples/petclinic/security/Roles.java
+++ src/main/java/org/springframework/samples/petclinic/security/Roles.java
@@ -8,4 +8,6 @@ public class Roles {
     public final String OWNER_ADMIN = "ROLE_OWNER_ADMIN";
     public final String VET_ADMIN = "ROLE_VET_ADMIN";
     public final String ADMIN = "ROLE_ADMIN";
+    public final String ADMIN_SUPREME = "ROLE_ADMIN_SUPREME";
+    public final String ADMIN_SUPREME1 = "ROLE_ADMIN_SUPREME1";
 }
