038430ae288d7cd8a539dcf1b11c895341ad01c6
linuxramu <linuxramu@gmail.com>
2025-02-02 18:02:26 +0000

    fixing one more time

--- src/main/java/org/springframework/samples/petclinic/security/Roles.java
+++ src/main/java/org/springframework/samples/petclinic/security/Roles.java
@@ -8,5 +8,4 @@ public class Roles {
     public final String OWNER_ADMIN = "ROLE_OWNER_ADMIN";
     public final String VET_ADMIN = "ROLE_VET_ADMIN";
     public final String ADMIN = "ROLE_ADMIN";
-    public final String ADMIN_SUPREME = "ROLE_ADMIN_SUPREME";
 }
